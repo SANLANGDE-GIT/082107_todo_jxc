7e2d6563418a01b8d29daa9d32ddf7bebfdd7212
gitee-sanlangde <1292303431@qq.com>
2021-03-09 10:46:54 +0800

    一、4.2退货单商品信息展示

--- src/main/java/com/atguigu/jxc/dao/ReturnListDao.java
+++ src/main/java/com/atguigu/jxc/dao/ReturnListDao.java
@@ -1,15 +1,18 @@
 package com.atguigu.jxc.dao;
 
-import com.atguigu.jxc.entity.ReturnListGoods;
+import com.atguigu.jxc.entity.PurchaseListGoods;
+import com.atguigu.jxc.entity.ReturnList;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface ReturnListDao {
 
-    List<ReturnListGoods> list(@Param("returnNumber") String returnNumber,
-                               @Param("supplierId") Integer supplierId,
-                               @Param("state") Integer state,
-                               @Param("t1") String sTime,
-                               @Param("t2") String eTime);
+    List<ReturnList> list(@Param("returnNumber") String returnNumber,
+                          @Param("supplierId") Integer supplierId,
+                          @Param("state") Integer state,
+                          @Param("t1") String sTime,
+                          @Param("t2") String eTime);
+
+    List<PurchaseListGoods> listGoods(@Param("returnListId") Integer returnListId);
 }
